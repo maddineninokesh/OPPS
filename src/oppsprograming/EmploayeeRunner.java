4172089490d3dcc7e8820b7593961f90f4ae5ead
maddi <maddi@NOKI>
2023-08-16 16:12:05 +0530

    new

---
+++ src/oppsprograming/EmploayeeRunner.java
@@ -0,0 +1,16 @@
+package oppsprograming;
+
+public class EmploayeeRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee em= new Employee();
+		em.setEmail("123@gmail.com");
+		em.setEmployerName("Nokesh");
+
+		
+		System.out.println(em);
+	}
+
+}
---
+++ src/oppsprograming/Employee.java
@@ -0,0 +1,43 @@
+package oppsprograming;
+
+import java.math.BigDecimal;
+
+public class Employee extends Person{
+	
+	private String title;
+	private String employerName;
+	private char employeeGrade;
+	private BigDecimal salary;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEmployerName() {
+		return employerName;
+	}
+	public void setEmployerName(String employerName) {
+		this.employerName = employerName;
+	}
+	public char getEmployeeGrade() {
+		return employeeGrade;
+	}
+	public void setEmployeeGrade(char employeeGrade) {
+		this.employeeGrade = employeeGrade;
+	}
+	public BigDecimal getSalary() {
+		return salary;	
+	}
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return super.toString()+
+				"Employee [title=" + title + ", employerName=" + employerName + ", employeeGrade=" + employeeGrade + " + email]";
+	}
+	
+	
+
+}
